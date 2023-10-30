afa334f6c3999cf663b1854228011f2417c59725
MuhammadAdi123 <149349848+MuhammadAdi123@users.noreply.github.com>
2023-10-30 11:52:59 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan18;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\nKelipatan 3.5 sampai dengan 35");
+        System.out.println("==============================");
+        for (double i = 3.5; i <= 35; i += 3.5) {
+                System.out.println(i);
+            }    // TODO code application logic here
+    }
+    
+}
